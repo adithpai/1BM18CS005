56511a29dadb70aea886c69f6f49f334bbe247ec
adithpai <53935446+adithpai@users.noreply.github.com>
2019-08-20 10:46:27 +0530

    Add files via upload

---
+++ postfix.c
@@ -0,0 +1,10 @@
+#include<iostream.h>
+void main()
+{
+ char ch[20];
+ int top=-1,precendence;
+ printf("Enter the expression");
+ scanf("%c",&ch);
+ for(i=0;i,ch.length;i++)
+ {
+  if(ch[i]=='^')//checking precedence
